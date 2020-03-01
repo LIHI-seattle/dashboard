d5f3010976cb0cd7bf53a673a6c587e659eacafe
donloby <donloby@uw.edu>
2020-03-01 12:22:26 -0800

    Add initial server file

---
+++ server/index.js
@@ -0,0 +1,9 @@
+"use strict";
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+
+app.listen(3000, () => {
+	console.log("server is listening on port 3000");
+});
+

76a10e7d48f1e4459db177ce087127e01854ebb8
aashnasheth <aashnas@cs.washington.edu>
2020-03-27 15:48:31 -0700

    Fixed button functionality

--- src/App.js
+++ src/App.js
@@ -8,39 +8,45 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      contacts: [],
-      viewId: 0
+      //contacts: [],
+      resDir: false,
+      roomTrack: false
     };
   }
 
   //api call: https://pusher.com/tutorials/consume-restful-api-react
-  componentDidMount() {
+ /* componentDidMount() {
     fetch('http://jsonplaceholder.typicode.com/users')
         .then(res => res.json())
         .then((data) => {
           this.setState({ contacts: data }) //as soon as comp is up and running will change state
         })
         .catch(console.log)
-  }
+  }*/
 
   //routing stuff: https://www.kirupa.com/react/creating_single_page_app_react_using_react_router.htm
-    onClick = (e) => {
-      //console.log("/" + e.target.value + "/");
-      //return  <Redirect  to={"/" + e.target.value} />
-       return (<div>
-           <ResidentDirectory contacts={this.state.contacts}/>
-       </div>);
+    onRoomClick = () => {
+      this.setState({roomTrack: true} )
+    };
 
-    }
+    onResClick = () => {
+        this.setState({resDir: true} )
+    };
+
+    onBackClick = () => {
+        this.setState({roomTrack: false, resDir: false} )
+    };
 
   render () {
       // switch statement idea from: https://stackoverflow.com/questions/54188654/how-to-load-a-new-component-on-button-click-in-reactjs
             return (
                 <div>
-                    <h1>Welcome to the LiHi Information Database</h1>
-                    <button value="RoomTracker" onClick={this.onClick}>Room Tracker</button>
-                    <button value="ResidentDirectory" onClick={this.onClick}>Resident Directory</button>
-                    <ResidentDirectory contacts={this.state.contacts}/>
+                    {!this.state.roomTrack && !this.state.resDir && <h1>Welcome to the LiHi Information Database</h1> }
+                    {!this.state.roomTrack && !this.state.resDir && <button value="RoomTracker" onClick={this.onRoomClick}>Room Tracker</button>}
+                    {!this.state.roomTrack && !this.state.resDir && <button value="ResidentDirectory" onClick={this.onResClick}>Resident Directory</button>}
+                    {this.state.roomTrack && <RoomTracker onBack={this.onBackClick} contacts={this.state.contacts}/>}
+                    {console.log(this.state.resDir)}
+                    {this.state.resDir && <ResidentDirectory onBack={this.onBackClick} contacts={this.state.contacts}/>}
                 </div>
             );
 
--- src/RoomTracker.js
+++ src/RoomTracker.js
@@ -1,12 +1,33 @@
 import React, {Component} from 'react';
 
 class RoomTracker extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            contacts: []
+        };
+    }
+
+    componentDidMount() {
+        fetch('http://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then((data) => {
+                this.setState({ contacts: data }) //as soon as comp is up and running will change state
+            })
+            .catch(console.log)
+    }
+
+    back = () => {
+      this.props.onBack()
+    };
+
     render() {
         return (
             <div>
+                <button onClick={this.back}>Back</button>
                 <h1>Room Tracking System</h1>
                 <h2> List of Available Rooms </h2>
-                    {this.props.contacts.map((village) => ( //-- key={item} --> below in list... why?
+                    {this.state.contacts.map((village) => ( //-- key={item} --> below in list... why?
                         // displaying lists https://www.robinwieruch.de/react-list-component
                         <div className="card">
                             <div className="card-body">

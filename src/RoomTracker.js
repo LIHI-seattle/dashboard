dbb0e0bf9311ee70944355fb6ce92b8bf735667b
aashnasheth <aashnas@cs.washington.edu>
2020-04-08 14:43:39 -0700

    Revert "Merge branch 'master' into frontend"
    This reverts commit b05de9e6e306d452c3c5ce4b16eb369854d45552, reversing
    changes made to cd419970d8036d9713c049415bbfe3b5e1c23244.

---
+++ src/RoomTracker.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+
+class RoomTracker extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            contacts: []
+        };
+    }
+
+    componentDidMount() {
+        fetch('http://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then((data) => {
+                this.setState({contacts: data}) //as soon as comp is up and running will change state
+            })
+            .catch(console.log)
+    }
+
+    back = () => {
+        this.props.onBack()
+    };
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.back}>Back</button>
+                <h1>Room Tracking System</h1>
+                <h2> List of Available Rooms </h2>
+                {this.state.contacts.map((village) => ( //-- key={item} --> below in list... why?
+                    // displaying lists https://www.robinwieruch.de/react-list-component
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 class="card-title">Village: {village.name}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Building: {village.email}</h6> {/* will be come village.building */}
+                            <p className="card-text">Vacant
+                                Rooms: {village.company.catchPhrase}</p> {/* will be come village.building.rooms */}
+                            {/* consider how to display various rooms, later? */}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+}
+
+export default RoomTracker;

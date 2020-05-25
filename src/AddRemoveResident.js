22ce8a637f5966cddd6ab7df669af74db39282f7
Mikey Wilson <wilsmic2@cs.washington.edu>
2020-05-25 13:28:35 -0700

    adding bootstrap to AddResident form

--- src/AddRemoveResident.js
+++ src/AddRemoveResident.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import { Grid, Row, Col } from 'react-bootstrap'
 
 
 export default class AddRemoveResident extends Component {
